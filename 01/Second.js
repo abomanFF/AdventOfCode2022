bfcafd4b38b13b925686f706507175933e497d87
ffAndre <andre.boman@gmail.com>
2022-12-01 20:57:56 +0100

    cleanup

--- 01/Second.js
+++ 01/Second.js
@@ -2,15 +2,11 @@
     Browser console solution, go to https://adventofcode.com/2022/day/1/input.
     Paste this in the console and you will get the answer for part 2 of day 1.
 */
-let dreamteamWeight = 0;
-let elfs = input
+document
   .getElementsByTagName("pre")[0]
   .textContent.split("\n\n")
-  .map((arr) => arr.split("\n").reduce((a, b) => (b ? a + parseInt(b) : a), 0));
-
-for (let i = 0; i < 3; i++) {
-  dreamteamWeight =
-    dreamteamWeight + parseInt(elfs.splice(elfs.indexOf(Math.max(...elfs)), 1));
-}
-
-console.log(dreamteamWeight);
+  .map((arr) => arr.split("\n").reduce((a, b) => (b ? a + parseInt(b) : a), 0))
+  .sort((a, b) => a - b)
+  .reverse()
+  .splice(0, 3)
+  .reduce((a, b) => a + b, 0);
