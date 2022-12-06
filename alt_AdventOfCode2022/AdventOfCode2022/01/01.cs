faaf8693dbb06624e86306bf1ebef4b11041965b
ffAndre <andre.boman@gmail.com>
2022-12-06 22:55:21 +0100

    csharp alternative

---
+++ alt_AdventOfCode2022/AdventOfCode2022/01/01.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode2022.Base;
+
+namespace AdventOfCode2022._01;
+
+public class Tests : BaseTest
+{
+    private int Solve(string input) => input
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(l => l.Split($"{Environment.NewLine}")
+                .Where(n => n != String.Empty).Select(n => int.Parse(n))
+                .Sum())
+            .Max();
+
+    [Test]
+    public void SolveTestData()
+    {
+        Assert.That(Solve(TestData), Is.EqualTo(24000));
+    }
+
+    [Test]
+    public void SolveData()
+    {
+        Assert.That(Solve(Data), Is.EqualTo(72070));
+    }
+}
---
+++ alt_AdventOfCode2022/AdventOfCode2022/Base/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AdventOfCode2022.Base;
+
+public abstract class BaseTest
+{
+    protected string Data { get; private set; }
+    protected string TestData { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        var folder = this.GetType()?.Namespace?.Split(".")[1].Replace("_", "") ?? String.Empty;
+        Data = File.ReadAllText($"{folder}/Data.in");
+        TestData = File.ReadAllText($"{folder}/TestData.in");
+    }
+
+    [Test]
+    public void DataIsNotNullOrEmpty()
+    {
+        Assert.That(Data, Is.Not.Null);
+        Assert.That(Data, Is.Not.EqualTo(String.Empty));
+    }
+
+    [Test]
+    public void TestDataIsNotNullOrEmpty()
+    {
+        Assert.That(TestData, Is.Not.Null);
+        Assert.That(TestData, Is.Not.EqualTo(String.Empty));
+    }
+}
