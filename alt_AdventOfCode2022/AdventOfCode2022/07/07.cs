5dad4ff208d129a7b1b9349af5d28c40c3672f22
André Boman <andre.boman@teliacompany.com>
2022-12-09 11:55:25 +0100

    day 8

--- alt_AdventOfCode2022/AdventOfCode2022/07/07.cs
+++ alt_AdventOfCode2022/AdventOfCode2022/07/07.cs
@@ -6,39 +6,55 @@ public class Day7 : BaseTest
 {
     private Dictionary<string, int> GetDirectoriesWithSizes(string input)
     {
+        //Split the input data into command chunks
         var commandList = input.Split($"{Environment.NewLine}$")
             .Select(l => l.Trim().Split(Environment.NewLine))
             .ToList();
 
+        //The available directories 
         var directories = new Dictionary<string, int>();
+
+        //Directories currently in scope
         var activeDirectories = new List<string>();
 
         commandList.ForEach(c =>
         {
+            //First line is always the command
             var command = c[0];
+
+            //If directory
             if (command.StartsWith("cd"))
             {
                 var thisDirectory =  command.Split("cd ")[1];
+                //Move back 1 step in the active directories
                 if (thisDirectory == "..")
                 {
                     activeDirectories.RemoveAt(activeDirectories.Count - 1);
                     return;
                 }
+                //Add full path to name to avoid collision with other files with same name
                 if (thisDirectory != "/")
                     thisDirectory = $"{activeDirectories[activeDirectories.Count - 1]}{thisDirectory}/";
 
+                //Add directory to available directories if not already added
                 if (!directories.ContainsKey(thisDirectory)) 
                     directories.Add(thisDirectory, 0);
 
+                //Add directory to the active directories currently in scope
                 activeDirectories.Add(thisDirectory);
                 return;
             }
 
+            //If not cd then it is a list(ls) command
+            //Itterate the directories and files listed by the command
             c[1..].ToList().ForEach(content =>
             {
+                //If directory, do nothing
                 if (content.StartsWith("dir"))
                     return;
 
+                //File data (1234 Filename.txt), split to access size
+                //Add size to directory and all parent directories
                 var fileData = content.Split(" ");
                 activeDirectories.ForEach(d => directories[d] = directories[d] + int.Parse(fileData[0]));
             });
---
+++ alt_AdventOfCode2022/AdventOfCode2022/08/08.cs
@@ -0,0 +1,144 @@
+﻿using AdventOfCode2022.Base;
+using Newtonsoft.Json.Linq;
+
+namespace AdventOfCode2022._08;
+
+public class Day8 : BaseTest
+{
+    public int Solve(string input)
+    {
+
+        var grid = input.Split(Environment.NewLine)
+            .Select(l => l.ToArray()
+                .Select(c => int.Parse(c.ToString())).ToList())
+            .ToList();
+
+        //All outside trees are visible
+        var visibleTrees = (grid.Count() * 2) + (grid.First().Count() * 2) - 4;
+
+        //For each row in grid
+        for (int i = 1; i < grid.Count()-1; i++)
+        {
+            var currentRow = grid[i].ToList();
+            //For each column
+            for(int j = 1; j < currentRow.Count()-1; j++)
+            {
+                var leftSmaller = currentRow.GetRange(0, j).FirstOrDefault(c => c >= currentRow[j], -1) == -1;
+                if (leftSmaller)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                var rightSmaller = currentRow.GetRange(j+1, currentRow.Count()-1-j).FirstOrDefault(c => c >= currentRow[j], -1) == -1;
+                if (rightSmaller)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                var topSmaller = grid.GetRange(0, i).FirstOrDefault(topRow => topRow[j] >= currentRow[j], null) == null;
+                if (topSmaller)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                var bottomSmaller = grid.GetRange(i+1, grid.Count()-1-i).FirstOrDefault(bottomRow =>  bottomRow[j] >= currentRow[j], null) == null;
+                if (bottomSmaller)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+            }
+        }
+
+        return visibleTrees;
+    }
+
+    public int Solve2(string input)
+    {
+
+        var grid = input.Split(Environment.NewLine)
+            .Select(l => l.ToArray()
+                .Select(c => int.Parse(c.ToString())).ToList())
+            .ToList();
+
+        //Highest scenic score
+        var savedScore = 0;
+
+        //For each row in grid
+        for (int i = 1; i < grid.Count() - 1; i++)
+        {
+            var currentRow = grid[i].ToList();
+            //For each column
+            for (int j = 1; j < currentRow.Count() - 1; j++)
+            {
+                var leftVal = 0;
+                var leftCells = currentRow.GetRange(0, j);
+                leftCells.Reverse();
+                foreach (var leftCell in leftCells)
+                {
+                    leftVal++;
+                    if (leftCell >= currentRow[j])
+                        break;
+                }
+
+                var rightVal = 0;
+                foreach(var rightCell in currentRow.GetRange(j + 1, currentRow.Count() - 1 - j))
+                {
+                    rightVal++;
+                    if (rightCell >= currentRow[j])
+                        break;
+                }
+
+                var topVal = 0;
+                var topCells = grid.GetRange(0, i);
+                topCells.Reverse();
+                foreach (var topRow in topCells)
+                {
+                    topVal++;
+                    if (topRow[j] >= currentRow[j])
+                        break;
+                }
+
+                var bottomVal = 0;
+                foreach (var bottomRow in grid.GetRange(i + 1, grid.Count() - 1 - i))
+                {
+                    bottomVal++;
+                    if (bottomRow[j] >= currentRow[j])
+                        break;
+                }
+
+                var scenicScore = leftVal * rightVal * topVal * bottomVal;
+                savedScore = scenicScore > savedScore ? scenicScore : savedScore;
+            }
+        }
+
+        return savedScore;
+    }
+
+    [Test]
+    public void SolveTestData()
+    {
+        Assert.That(Solve(TestData), Is.EqualTo(21));
+    }
+
+    [Test]
+    public void SolveDataPart1()
+    {
+        Assert.That(Solve(Data), Is.EqualTo(1693));
+    }
+
+    [Test]
+    public void SolveTestDataPart2()
+    {
+        Assert.That(Solve2(TestData), Is.EqualTo(8));
+    }
+
+    [Test]
+    public void SolveDataPart2()
+    {
+        Assert.That(Solve2(Data), Is.EqualTo(1693));
+    }
+}
