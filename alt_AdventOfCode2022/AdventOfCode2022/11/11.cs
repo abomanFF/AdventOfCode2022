ea5e8a2cf131a2fc69949a0e98d704fccbc686d7
André Boman <andre.boman@teliacompany.com>
2022-12-15 08:31:25 +0100

    day 11

---
+++ alt_AdventOfCode2022/AdventOfCode2022/11/11.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2022._11._11;
+using AdventOfCode2022.Base;
+using System.Numerics;
+
+namespace AdventOfCode2022._11;
+
+public class Day11 : BaseTest
+{
+    [Test]
+    public void SolveTestData()
+    {
+        var data = new MonkeyItemInspectors().Solve(TestData, nrOfRounds: 10000);
+        Assert.That(new MonkeyItemInspectors().Solve(TestData, nrOfRounds: 20, worryDivider: 3), Is.EqualTo(10605));
+        Assert.That(data, Is.EqualTo(2713310158));
+    }
+
+    [Test]
+    public void SolveData()
+    {
+        Assert.That(new MonkeyItemInspectors().Solve(Data, nrOfRounds: 20, worryDivider: 3), Is.EqualTo(57838));
+        Assert.That(new MonkeyItemInspectors().Solve(Data, nrOfRounds: 10000), Is.EqualTo(15050382231));
+    }
+
+}
---
+++ alt_AdventOfCode2022/AdventOfCode2022/11/11/MonkeyItemInspectors.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022._11._11;
+
+
+public class Monkey
+{
+    public Monkey(string rawMonkey)
+    {
+        var monkeyRows = rawMonkey.Split(Environment.NewLine);
+        Items = monkeyRows[1].Split(": ")[1].Split(", ").Select(d => long.Parse(d)).ToList();
+        Operation = monkeyRows[2].Split("new = ")[1];
+        Divisible = int.Parse(monkeyRows[3].Split("divisible by ")[1]);
+        TrueMonkey = int.Parse(monkeyRows[4].Split(" ").Last());
+        FalseMonkey = int.Parse(monkeyRows[5].Split(" ").Last());
+        InspectedItemCount = 0;
+    }
+    public List<long> Items { get; set; }
+    public string Operation { get; set; }
+    public int Divisible { get; set; }
+    public int TrueMonkey { get; set; }
+    public int FalseMonkey { get; set; }
+    public int InspectedItemCount { get; set; }
+    public long RunOperation(long value, int factor)
+    {
+        InspectedItemCount++;
+
+        var vals = Operation.Split(" ");
+        long val = vals[0] == "old" ? value : int.Parse(vals[0]);
+        long val2 = vals[2] == "old" ? value : int.Parse(vals[2]);
+        return (vals[1] == "+" ? (val + val2) : (val * val2)) % factor;
+    }
+
+}
+
+public class MonkeyItemInspectors
+{
+    public long Solve(string input, int nrOfRounds, long worryDivider = 1)
+    {
+        Console.WriteLine(long.MaxValue);
+
+        var monkeys = input.Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(rm => new Monkey(rm))
+            .ToList();
+
+        var factor = monkeys.Aggregate(1, (a, b) => a * b.Divisible);
+
+        for(var rounds = 0; rounds < nrOfRounds; rounds++)
+        {
+            monkeys.ForEach(m =>
+            {
+                foreach(var item in new List<long>(m.Items))
+                {
+                    var worryLevel = m.RunOperation(item, factor);
+                    if (worryDivider > 1)
+                        worryLevel = worryLevel / worryDivider;
+
+                    var passTo = worryLevel % m.Divisible == 0 ? m.TrueMonkey : m.FalseMonkey;
+                    m.Items.Remove(item);
+                    monkeys[passTo].Items.Add(worryLevel);
+                }
+            });
+        }
+      
+        monkeys = monkeys.OrderByDescending(m => m.InspectedItemCount).ToList();
+        return Math.BigMul(monkeys[0].InspectedItemCount, monkeys[1].InspectedItemCount);
+    }
+
+}
